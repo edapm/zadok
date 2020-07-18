56e4e0cf182741efeec6bee45522d8319205d9c0
Edward-Mason <mynameisprivate3@gmail.com>
2020-07-18 12:18:54 +0100

    Debugging

--- zadok.py
+++ zadok.py
@@ -2,7 +2,7 @@ from random import *
 from time import *
 
 # Set up vars
-number = raw_input("What's your number? ")
+number = input("What's your number? ")
 # Change input to integer and print number+1
 number = int(number)
 print (number+1)
