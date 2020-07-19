23c77309c77f6842cdf6a65332012f8979da08f4
Edward-Mason <mynameisprivate3@gmail.com>
2020-07-19 15:00:50 +0100

    Remove imports

--- zadok.py
+++ zadok.py
@@ -1,12 +1,9 @@
-from random import *
-from time import *
-
 # define todo
 todo = ""
 # allow user to add to-do items
 while todo != "exit":
     todo = input("Enter item ('exit' to exit): ")
     if todo != "exit":
-        file = open("todo.md","a+")
+        file = open("todo.md", "a+")
         file.write("\n"+"- [ ] "+todo)
         file.close()
