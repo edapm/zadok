4ffe44ef3c13cc1c4adf8d866f44f809a82a8547
Edward-Mason <mynameisprivate3@gmail.com>
2020-07-23 17:49:32 +0100

    merge text and newline

--- zadok.py
+++ zadok.py
@@ -5,5 +5,5 @@ while todo != "exit":
     todo = input("Enter item ('exit' to exit): ")
     if todo != "exit":
         file = open("todo.md", "a+")
-        file.write("\n"+"- [ ] "+todo)
+        file.write("\n- [ ] "+todo)
         file.close()
