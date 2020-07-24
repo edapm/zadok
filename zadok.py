5a19571b54d935d2d1bda3b743ef47baea50f3a3
Edward-Mason <mynameisprivate3@gmail.com>
2020-07-24 18:14:24 +0100

    Begin to debug issue #1

--- zadok.py
+++ zadok.py
@@ -3,13 +3,13 @@ todo = ""
 # allow user to add to-do items
 while todo != "exit":
     todo = input("Enter item ('exit' to exit): ")
-    if todo != "exit":
-        file = open("todo.md", "a+")
-        file.write("\n- [ ] "+todo)
-        display = input("Do you want to display your list? (y/n) ")
-        if display == "y":
-            list = file.readlines()
-            file.close()
-            print(list)
-        else:
-            file.close()
+#    if todo != "exit":
+    file = open("todo.md", "a+")
+    file.write("\n - [ ] "+todo)
+    display = input("Do you want to display your list? (y/n) ")
+    if display == "y":
+        list = file.readlines()
+        file.close()
+        print(list)
+    else:
+        file.close()
