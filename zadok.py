36330a0733a3edcba4d18068e94e84498807ddc7
Edward-Mason <mynameisprivate3@gmail.com>
2020-07-24 20:55:59 +0100

    This is why I strongly dislike Python

--- zadok.py
+++ zadok.py
@@ -1,15 +1,19 @@
 # define todo
-todo = ""
+# todo = ""
 # allow user to add to-do items
-while todo != "exit":
-    todo = input("Enter item ('exit' to exit): ")
-#    if todo != "exit":
-    file = open("todo.md", "a+")
-    file.write("\n - [ ] "+todo)
-    display = input("Do you want to display your list? (y/n) ")
-    if display == "y":
-        list = file.readlines()
-        file.close()
-        print(list)
-    else:
-        file.close()
+# while todo != "exit":
+    # todo = input("Enter item ('exit' to exit): ")
+    # if todo != "exit":
+        # input = input("if todo != exit:")
+file = open("todo.md", "a+")
+        #input = input("file open")
+        #file.write("\n - [ ] "+todo)
+        #input = input("file write")
+file.close()
+        #input = input("file close")
+        #display = input("Do you want to display your list? (y/n) ")
+        #if display == "y":
+        #    file = open("todo.md", "r")
+        #    list = file.readlines()
+        #    file.close()
+        #    print(list)
