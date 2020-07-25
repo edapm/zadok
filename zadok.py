4374a7ce65546af1c7e43c3d281106f8616af2b3
Edward-Mason <mynameisprivate3@gmail.com>
2020-07-25 15:45:52 +0100

    Debugging of issue #1 - file opening not the problem

--- zadok.py
+++ zadok.py
@@ -1,19 +1,23 @@
+import time
 # define todo
-# todo = ""
+todo = ""
 # allow user to add to-do items
-# while todo != "exit":
-    # todo = input("Enter item ('exit' to exit): ")
-    # if todo != "exit":
-        # input = input("if todo != exit:")
-file = open("todo.md", "a+")
-        #input = input("file open")
-        #file.write("\n - [ ] "+todo)
-        #input = input("file write")
-file.close()
-        #input = input("file close")
-        #display = input("Do you want to display your list? (y/n) ")
-        #if display == "y":
-        #    file = open("todo.md", "r")
-        #    list = file.readlines()
-        #    file.close()
-        #    print(list)
+while todo != "exit":
+    todo = input("Enter item ('exit' to exit): ")
+    if todo != "exit":
+        print("if todo != exit:")
+        time.sleep(2)
+        file = open("todo.md", "a+")
+        print("file open")
+        file.write("\n - [ ] "+todo)
+        print("file write")
+        time.sleep(2)
+        file.close()
+        print("file close")
+        time.sleep(2)
+        display = input("Do you want to display your list? (y/n) ")
+        if display == "y":
+            file = open("todo.md", "r")
+            list = file.readlines()
+            file.close()
+            print(list)
