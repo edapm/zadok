1c117e2a2d4cb7009e89bdd063475bf23de2c595
Edward-Mason <mynameisprivate3@gmail.com>
2020-07-25 16:40:26 +0100

    v0.2

--- zadok.py
+++ zadok.py
@@ -1,23 +1,13 @@
-import time
-# define todo
-todo = ""
-# allow user to add to-do items
-while todo != "exit":
-    todo = input("Enter item ('exit' to exit): ")
-    if todo != "exit":
-        print("if todo != exit:")
-        time.sleep(2)
-        file = open("todo.md", "a+")
-        print("file open")
-        file.write("\n - [ ] "+todo)
-        print("file write")
-        time.sleep(2)
+while True:
+    todo = input('Enter item (exit to exit): ')
+    if todo == "exit":
+        break
+    file = open("todo.md", "a+")
+    file.write("\n - [ ] "+todo)
+    file.close()
+    display = input("Do you want to display your list? (y/n) ")
+    if display == "y":
+        file = open("todo.md", "r")
+        list = file.readlines()
         file.close()
-        print("file close")
-        time.sleep(2)
-        display = input("Do you want to display your list? (y/n) ")
-        if display == "y":
-            file = open("todo.md", "r")
-            list = file.readlines()
-            file.close()
-            print(list)
+        print(list)
