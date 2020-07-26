c18bb852d29b7a746a746282958f6f5f9795cde5
Edward-Mason <mynameisprivate3@gmail.com>
2020-07-26 16:23:41 +0100

    Add comments to code, begin to work on #7 part 2 (Finish todos)

--- zadok.py
+++ zadok.py
@@ -1,19 +1,29 @@
+# import modules
 from pathlib import Path
+# ask user where to store todo file
 fileOpen = Path(input("Enter file path (see docs for more info): "))
+# mainloop
 while True:
-    choices = "1 to add item, 2 to view list, 3 to exit"
+    # options of what to do
+    choices = "1 to add item, 2 to view list, 3 to finish a todo, 4 to exit"
     print(choices)
-    todo = input("Enter choice (1-3): ")
+    todo = input("Enter choice (1-4): ")
+    # option 1 -> add item
     if todo == "1":
         file = open(fileOpen, "a+")
         text = input("Enter item: ")
         file.write("\n - [ ] " + text)
         file.close()
+    # option 2 -> read list
     elif todo == "2":
-        file = open(fileOpen, "r")
+        file = open(fileOpen, "r+")
         list = file.readlines()
         file.close()
         print(list)
-    if todo == "3":
+    # option 3 -> finish a todo
+    elif todo == "3":
+        file = open(fileOpen, "a+")
+    # option 4 -> exit program
+    elif todo == "4":
         print("Bye!")
         break
