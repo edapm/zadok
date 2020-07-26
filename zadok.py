8593ebcbb7004a7b98ef3f25e0d9f2e64140a746
Edward-Mason <mynameisprivate3@gmail.com>
2020-07-26 14:59:20 +0100

    Add fileOpen with pathlib to specify path, #7

--- zadok.py
+++ zadok.py
@@ -1,20 +1,19 @@
+from pathlib import Path
+fileOpen = Path(input("Enter file path (see docs for more info): "))
 while True:
-    location = ""
-    choices = ["1 to change storage location", "2 to add item", "3 to view list", "4 to exit"]
+    choices = "1 to add item, 2 to view list, 3 to exit"
     print(choices)
-    todo = input("Enter choice (1-4): ")
+    todo = input("Enter choice (1-3): ")
     if todo == "1":
-        location = input("Enter file location (First time using? See docs for more info): ")
-    elif todo == "2":
-        file = open(location + "todo.md", "a+")
+        file = open(fileOpen, "a+")
         text = input("Enter item: ")
         file.write("\n - [ ] " + text)
         file.close()
-    elif todo == "3":
-        file = open(location + "todo.md", "r")
+    elif todo == "2":
+        file = open(fileOpen, "r")
         list = file.readlines()
         file.close()
         print(list)
-    if todo == "4":
+    if todo == "3":
         print("Bye!")
         break
